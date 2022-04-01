b51f48c00d4e17b27f3a801a520e02113ae94378
douglas.morais@i-value.com.br <douglas.morais@i-value.com.br>
2022-04-01 20:27:38 -0300

    update api conect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,55 @@
+import { useState } from "react";
+import {  api } from './service/api'
+
 const App = () => {
+  const [ data, setData ] = useState({})
+  const [ showForm, setShowForm ] = useState(true)
+
+  const submitData = (e) => {
+    e.preventDefault();
+    api.post("", data).then(
+      res => {
+        if (res.status === 200 || 201){
+          setShowForm(false)
+        }
+      }
+    )
+  }
+
+  if(!showForm){
+    return(
+      <div>
+        <h2>Dados enviados com sucesso!</h2>
+      </div>
+    )
+  }
   return(
     <div>
       <h1>Hello World!!!</h1>
+      <form onSubmit={submitData}>
+        <input 
+          type="text" 
+          placeholder="Informe seu nome"
+          onChange={ e => setData({ ...data, name: e.target.value })}
+        />
+        <input 
+          type="text" 
+          placeholder="Informe seu telefone"
+          onChange={ e => setData({ ...data, phone: e.target.value })}
+        />
+        <input 
+          type="text" 
+          placeholder="Informe seu email"
+          onChange={ e => setData({ ...data, email: e.target.value })}
+        />
+        <input 
+          type="submit" 
+          value="Cadastrar"
+        />
+      </form>
+      <div>
+        <p>{data?.name}</p>
+      </div>
     </div>
   )
 }
---
+++ src/service/api.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export const api = axios.create({
+  baseURL: "https://webhook.site/4c639737-c1c9-488d-a085-013d2a26e614"
+})
